595b2d8374600258e6da90403540a50ed9f22940
Soomin Hwang <soomin.h.hwang@gmail.com>
2019-07-10 16:35:52 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,6 +1,6 @@
 #meal_choice
-def meal_choice(veg1, veg2, "meat")
-  puts "A plate of meat with #{veg1} and #{veg2}"
-  return "A plate of meat with #{veg1} and #{veg2}"
+def meal_choice(veg1, veg2, protein="meat")
+  puts "A plate of #{protein} with #{veg1} and #{veg2}"
+  return "A plate of #{protein} with #{veg1} and #{veg2}"
   return "What a nutritious meal!"
 end
